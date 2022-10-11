4482277268fd9c2d95e36a97dc9027c72dbb55c8
jiachangqin <115522734+jiachangqin@users.noreply.github.com>
2022-10-11 12:53:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'wxed9e69b06db46c7a',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '测试',
+      useTemplateId: '6518486094b9c03b766258ac723e44b2',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-26',
       festivals: [
