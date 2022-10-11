8d2125da8cbabb02f95d3d605c746b80043b08ae
jiachangqin <115522734+jiachangqin@users.noreply.github.com>
2022-10-11 13:05:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wxed9e69b06db46c7a',
+      id: 'oSNXp6a_WoeuDvKOHkHtl3fxIzjc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '6518486094b9c03b766258ac723e44b2',
+      useTemplateId: 'mvyIxagEQcsXs2W7EXEg0sz2LX4h27GGeaqBeV_R15c',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-26',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'mvyIxagEQcsXs2W7EXEg0sz2LX4h27GGeaqBeV_R15c',
 
   CALLBACK_USERS: [
     {
